39276874310cd6b950af18a6bb6f41f3d76909bd
erengthb <erndmrhn@gmail.com>
2022-06-16 23:50:52 +0300

    swagger ui add in to project

--- northwind/src/main/java/kodlamaio/northwind/NorthwindApplication.java
+++ northwind/src/main/java/kodlamaio/northwind/NorthwindApplication.java
@@ -2,12 +2,30 @@ package kodlamaio.northwind;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2  // swaggeri başlatan annotasyon
 public class NorthwindApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NorthwindApplication.class, args);
 	}
+	
+	
+	//swagger için gerekli konfigürasyon 
+	@Bean  //javada classa karşılık gelir 
+    public Docket api() {   // uygulama içindeki apileri bulup test edebileceğimiz bir noktaya taşır 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.basePackage("kodlamaio.northwind"))                                           
+          .build();                                           
+    }
 
 }
