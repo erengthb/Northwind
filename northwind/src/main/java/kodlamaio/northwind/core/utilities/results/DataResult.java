8d0a78914218e44f13e127c90822925f88288259
erengthb <erndmrhn@gmail.com>
2022-06-19 00:02:07 +0300

    Error and Succes Result writed

---
+++ northwind/src/main/java/kodlamaio/northwind/core/utilities/results/DataResult.java
@@ -0,0 +1,32 @@
+package kodlamaio.northwind.core.utilities.results;
+
+public class DataResult<T> extends Result {  //birden fazla veri tipi olabileceğiiçin generic <T> classla çalışılmalı
+
+
+    private T data ;
+    
+	public DataResult( T data , boolean success, String message) {
+		super(success, message); // base sınıfın constructorlarını çalıştırmaya yarar
+		
+		this.data=data;
+		
+		
+	}
+	
+	public DataResult( T data , boolean success ) {
+		super(success); // base sınıfın constructorlarını çalıştırmaya yarar
+		
+		this.data=data;
+		
+		
+	}
+	
+	public T getData() {
+		
+		return this.data;
+		
+	}
+
+	
+
+}
